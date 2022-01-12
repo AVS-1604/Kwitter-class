8f3765f7614f17d81a9f29823a0c27c6365701ed
AVS-1604 <91470514+AVS-1604@users.noreply.github.com>
2022-01-12 17:43:31 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -27,3 +27,13 @@ function logout()
       localStorage.removeItem("room_name");
         window.location = "index.html"
 }
+function send()
+{
+      msg = document.getElementById("msg").value;
+      firebaseConfig.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+      });
+      document.getElementById("msg").value = "";
+}
--- kwitter_room.js
+++ kwitter_room.js
@@ -32,19 +32,4 @@ function redirectToRoomName(name)
         window.location = "kwitter_page.html";
 }
 
-function logout()
-{
-      localStorage.removeItem("user_name");
-      localStorage.removeItem("room_name");
-        window.location = "index.html"
-}
-function send()
-{
-      msg = document.getElementById("msg").value;
-      firebaseConfig.database().ref(room_name).push({
-            name:user_name,
-            message:msg,
-            like:0
-      });
-      document.getElementById("msg").value = "";
-}
+
