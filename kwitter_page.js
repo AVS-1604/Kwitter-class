cf08197c36cc9052bd4bb51dd51cd88240d8aa9c
AVS-1604 <91470514+AVS-1604@users.noreply.github.com>
2022-01-12 17:38:37 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,4 +1,16 @@
 //YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyCwIKh6NoPQc7ssecT5u-1HTaxaL8Z2gCU",
+      authDomain: "kwitter-whjr-eb865.firebaseapp.com",
+      databaseURL: "https://kwitter-whjr-eb865-default-rtdb.firebaseio.com",
+      projectId: "kwitter-whjr-eb865",
+      storageBucket: "kwitter-whjr-eb865.appspot.com",
+      messagingSenderId: "906224588292",
+      appId: "1:906224588292:web:96028a534630cf01f18751"
+    };
+    
+    // Initialize Firebase
+    var app = initializeApp(firebaseConfig);
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
@@ -8,3 +20,10 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
 //End code
       } });  }); }
 getData();
+
+function logout()
+{
+      localStorage.removeItem("user_name");
+      localStorage.removeItem("room_name");
+        window.location = "index.html"
+}
