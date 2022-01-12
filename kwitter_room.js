730ea0367537588b4c78280d2b7e27134db1ae5f
AVS-1604 <91470514+AVS-1604@users.noreply.github.com>
2022-01-12 17:29:11 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,16 +1,17 @@
 // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 var firebaseConfig = {
-      apiKey: "AIzaSyCwIKh6NoPQc7ssecT5u-1HTaxaL8Z2gCU",
-      authDomain: "kwitter-whjr-eb865.firebaseapp.com",
-      projectId: "kwitter-whjr-eb865",
-      storageBucket: "kwitter-whjr-eb865.appspot.com",
-      messagingSenderId: "906224588292",
-      appId: "1:906224588292:web:96028a534630cf01f18751"
-    };
-    
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-//ADD YOUR FIREBASE LINKS HERE
+  apiKey: "AIzaSyCwIKh6NoPQc7ssecT5u-1HTaxaL8Z2gCU",
+  authDomain: "kwitter-whjr-eb865.firebaseapp.com",
+  databaseURL: "https://kwitter-whjr-eb865-default-rtdb.firebaseio.com",
+  projectId: "kwitter-whjr-eb865",
+  storageBucket: "kwitter-whjr-eb865.appspot.com",
+  messagingSenderId: "906224588292",
+  appId: "1:906224588292:web:96028a534630cf01f18751"
+};
+
+// Initialize Firebase
+var app = initializeApp(firebaseConfig);
 
 function addRoom()
 {
