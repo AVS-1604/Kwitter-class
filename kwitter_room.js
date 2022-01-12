edadb70bbecaf85c0887f846bc2dddc9e18128e3
AVS-1604 <91470514+AVS-1604@users.noreply.github.com>
2022-01-12 17:46:07 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -2,6 +2,18 @@
 // Your web app's Firebase configuration
 
 //ADD YOUR FIREBASE LINKS HERE
+var firebaseConfig = {
+      apiKey: "AIzaSyCwIKh6NoPQc7ssecT5u-1HTaxaL8Z2gCU",
+      authDomain: "kwitter-whjr-eb865.firebaseapp.com",
+      databaseURL: "https://kwitter-whjr-eb865-default-rtdb.firebaseio.com",
+      projectId: "kwitter-whjr-eb865",
+      storageBucket: "kwitter-whjr-eb865.appspot.com",
+      messagingSenderId: "906224588292",
+      appId: "1:906224588292:web:96028a534630cf01f18751"
+    };
+    
+    // Initialize Firebase
+    var app = initializeApp(firebaseConfig);
 
 function addRoom()
 {
