4c46767dd6062fbe67e7f050801a34445c6632e4
AVS-1604 <91470514+AVS-1604@users.noreply.github.com>
2022-01-20 15:45:02 +0530

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCwIKh6NoPQc7ssecT5u-1HTaxaL8Z2gCU",
+  authDomain: "kwitter-whjr-eb865.firebaseapp.com",
+  databaseURL: "https://kwitter-whjr-eb865-default-rtdb.firebaseio.com",
+  projectId: "kwitter-whjr-eb865",
+  storageBucket: "kwitter-whjr-eb865.appspot.com",
+  messagingSenderId: "906224588292",
+  appId: "1:906224588292:web:96028a534630cf01f18751"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 function addUser() {
     user_name = document.getElementById("user_name").value;
 
